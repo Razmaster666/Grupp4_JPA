389d352c3a183495823d8834b0427a2120d81c7a
RasmusClausen <rasmusclausen123@gmail.com>
2023-01-12 11:20:46 +0100

    asdasdasd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,3 @@
 INSERT INTO MOVIE (TITLE, IMAGE_NAME, GENRE, PRICE, DESCRIPTION)
-VALUES ('Avatar', 'avatar.jpg', );
-
---movies.add(new Movie("Avatar", 150, "Sci-Fi", "Avatar.jpg",
---"En före detta marinkårssoldat blir förälskad i en blåhyad krigare på en
---främmande planet. Han ställer sig på hennes folks sida i deras kamp mot människans övergrepp
---på deras frodiga planet."));
+VALUES ('Avatar', 'avatar.jpg', 'Sci-Fi', 150,
+'En före detta marinkårssoldat blir förälskad i en blåhyad krigare på en främmande planet. Han ställer sig på hennes folks sida i deras kamp mot människans övergrepp på deras frodiga planet')

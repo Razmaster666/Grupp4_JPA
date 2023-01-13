b1d078d82cf6f52d79af4877ae3f7d9129a695fe
MiaHolma <miaholma93@gmail.com>
2023-01-13 13:30:26 +0100

    Startsida oklart utseende

---
+++ src/main/java/com/example/movie/controllers/HomepageController.java
@@ -0,0 +1,33 @@
+package com.example.movie.controllers;
+
+import com.example.movie.models.Movie;
+import com.example.movie.models.Snack;
+import com.example.movie.repositories.MovieRepository;
+import com.example.movie.repositories.SnackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+//@RequestMapping("/homePage")
+@Controller
+public class HomepageController {
+
+    @Autowired
+    MovieRepository movieRepository;
+    @Autowired
+    SnackRepository snackRepository;
+
+    @GetMapping("/")
+    String getAll(Model model){
+        List<Movie> movieList= (List<Movie>) movieRepository.findAll();
+        List<Snack> snackList= (List<Snack>) snackRepository.findAll();
+        model.addAttribute("snacks", snackList);
+        model.addAttribute("movies", movieList);
+        return "homePage";
+    }
+
+}

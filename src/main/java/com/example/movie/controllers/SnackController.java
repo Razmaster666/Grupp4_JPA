99cee500157f80029cf75724c41e5d209822b84a
MiaHolma <miaholma93@gmail.com>
2023-01-13 13:01:58 +0100

    Gjort snack-html så den också har sin sida likt movies

--- src/main/java/com/example/movie/controllers/SnackController.java
+++ src/main/java/com/example/movie/controllers/SnackController.java
@@ -1,11 +1,13 @@
 package com.example.movie.controllers;
 
+import com.example.movie.models.Movie;
 import com.example.movie.models.Snack;
 import com.example.movie.repositories.SnackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,4 +26,13 @@ public class SnackController {
         model.addAttribute("snackList", snackList);
         return "snacks";
     }
+
+    @GetMapping("/{id}")
+    String getSnack(Model model, @PathVariable Long id){
+        Snack snack = snackRepository.findById(id).get();
+        model.addAttribute("snack",snack);
+        return "detailsSnack";
+    }
+
+
 }

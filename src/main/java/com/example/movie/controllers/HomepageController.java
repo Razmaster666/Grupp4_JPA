02e180a39fbf4ecad6802f072d6a3ef9cff23955
RasmusClausen <rasmusclausen123@gmail.com>
2023-01-16 15:18:35 +0100

    massa grejer

--- src/main/java/com/example/movie/controllers/HomepageController.java
+++ src/main/java/com/example/movie/controllers/HomepageController.java
@@ -8,10 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-@RequestMapping("/homePage")
+//@RequestMapping("/homePage")
 @Controller
 public class HomepageController {
 
@@ -20,7 +19,7 @@ public class HomepageController {
     @Autowired
     SnackRepository snackRepository;
 
-    @GetMapping("")
+    @GetMapping("/")
     String getAll(Model model){
         List<Movie> movieList= (List<Movie>) movieRepository.findAll();
         List<Snack> snackList= (List<Snack>) snackRepository.findAll();
@@ -28,19 +27,5 @@ public class HomepageController {
         model.addAttribute("movies", movieList);
         return "homePage";
     }
-
-//    @GetMapping("/movies/{id}")
-//    String getMovie(Model model, @PathVariable Long id){
-//        Movie movie = movieRepository.findById(id).get();
-//        model.addAttribute("movie",movie);
-//        return "detailsMovie";
-//    }
-//
-//    @GetMapping("/snacks/{id}")
-//    String getSnack(Model model, @PathVariable Long id){
-//        Snack snack = snackRepository.findById(id).get();
-//        model.addAttribute("snack",snack);
-//        return "detailsSnack";
-//    }
 
 }
